faa523ba2075552fa4517f6007979499020b5b25
Maksim Demianov <maks.demianov@gmail.com>
2019-07-25 17:50:01 +0300

    Initial commit

---
+++ src/main/java/com/study/filmography/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.study.filmography.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.study")
+public class WebConfig {
+
+    @Bean
+    ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
---
+++ src/main/webapp/pages/editPage.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Макс
+  Date: 25.07.2019
+  Time: 16:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+Edit page
+
+</body>
+</html>
