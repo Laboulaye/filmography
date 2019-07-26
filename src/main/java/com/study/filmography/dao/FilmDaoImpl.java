8331b8dfb608d4153511a59e03e80bb806c6b8bb
Maksim Demianov <maks.demianov@gmail.com>
2019-07-26 12:09:17 +0300

    Замена методов CRUD на работу с базой данных, а не с тренировочной коллекцией

--- src/main/java/com/study/filmography/dao/FilmDaoImpl.java
+++ src/main/java/com/study/filmography/dao/FilmDaoImpl.java
@@ -1,6 +1,9 @@
 package com.study.filmography.dao;
 
 import com.study.filmography.model.Film;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -12,46 +15,41 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Repository
 public class FilmDaoImpl implements FilmDao {
 
-    private static final AtomicInteger AUTO_ID = new AtomicInteger(0);
-    private static Map<Integer, Film> films = new HashMap<>();
+    private SessionFactory sessionFactory;
 
-    static{
-        Film film1 = new Film();
-        film1.setId(AUTO_ID.getAndIncrement());
-        film1.setTitle("Inception");
-        film1.setYear(2010);
-        film1.setGenre("sci-fi");
-        film1.setWatched(true);
-
-        films.put(film1.getId(), film1);
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<Film> allFilms() {
-        return new ArrayList<>(films.values());
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("From Film").list();
     }
 
     @Override
     public void add(Film film) {
-        film.setId(AUTO_ID.getAndIncrement());
-        films.put(film.getId(), film);
-
+        Session session = sessionFactory.getCurrentSession();
+        session.persist(film);
     }
 
     @Override
     public void delete(Film film) {
-        films.remove(film.getId());
-
+        Session session = sessionFactory.getCurrentSession();
+        session.delete(film);
     }
 
     @Override
     public void edit(Film film) {
-        films.put(film.getId(), film);
-
+        Session session = sessionFactory.getCurrentSession();
+        session.update(film);
     }
 
     @Override
     public Film getById(int id) {
-        return films.get(id);
-    }
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Film.class, id);
+}
 }
--- src/main/java/com/study/filmography/service/FilmServiceImpl.java
+++ src/main/java/com/study/filmography/service/FilmServiceImpl.java
@@ -5,6 +5,7 @@ import com.study.filmography.dao.FilmDaoImpl;
 import com.study.filmography.model.Film;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.xml.ws.ServiceMode;
 import java.util.List;
@@ -19,26 +20,31 @@ public class FilmServiceImpl implements FilmService {
     }
 
     @Override
+    @Transactional
     public List<Film> allFilms() {
         return filmDao.allFilms();
     }
 
     @Override
+    @Transactional
     public void add(Film film) {
         filmDao.add(film);
     }
 
     @Override
+    @Transactional
     public void delete(Film film) {
         filmDao.delete(film);
     }
 
     @Override
+    @Transactional
     public void edit(Film film) {
         filmDao.edit(film);
     }
 
     @Override
+    @Transactional
     public Film getById(int id) {
         return filmDao.getById(id);
     }
